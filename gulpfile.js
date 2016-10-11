6da6878db350a5cf6f830ed9e259dc9106364b21
Sean Cheung <theoxuanx@gmail.com>
2016-10-11 19:48:05 +0800

    semantic added but not built yet

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.less('app.less')
+       .webpack('app.js')
+       .copy('resources/assets/semantic/dist/themes', 'public/themes');
 });
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,7 +8,8 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
+/*require('bootstrap-sass');*/
+require(__dirname__ + '/../semantic/dist/semantic.js');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
